5c6eaf45d179371a03c6411711e2690a00f5a9a4
Carlos Antonino <carlosantonino.cs@gmail.com>
2022-05-19 13:46:27 -0300

    começo de projeto react

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react'
 export default class header extends Component {
   render() {
     return (
-      <div>
-          <h1>Inicio</h1>
-      </div>
+      <div>header</div>
     )
   }
 }
